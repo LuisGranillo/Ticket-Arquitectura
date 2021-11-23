36464a2fec363384c7d19251f8470bcfeb1a1282
LuisGranillo <79174029+LuisGranillo@users.noreply.github.com>
2021-11-22 18:18:59 -0600

    Update Index.php

--- USUARIO COMUN/TICKETS/Index.php
+++ USUARIO COMUN/TICKETS/Index.php
@@ -51,10 +51,13 @@ $si2=0;
                 <meta charset="UTF-8">
                 <title>SubDirección de Educación a distancia</title>
                 <link rel="stylesheet" type="text/css" href="../CSS/Style.css">
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
                 <script type="text/javascript" src="~/Scripts/bootstrap.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
                 <link type="text/css" rel="stylesheet" href="~/Content/bootstrap.min.css"/>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         </head>
         <body>      
                      <header>
@@ -160,12 +163,111 @@ $si2=0;
                                        
                                 </article>
                                 <article>
-
+                                <button class="btn btn-info mensajeConstruccion" data-toggle="modal" data-target="#miventana">
+				Aviso de privacidad
+			</button>
                                         <h2> ¡BIENVENIDO AL CENTRO DE SOPORTE !</h2>
                                            <p>El objetivo del sistema de Centro de Soporte es llevar un registro detallado de las problemáticas y solicitudes de los usuarios de la Subdirección de Educación a Distancia, como parte del soporte técnico que brindan cada una de las áreas que la integran, para ofrecer una respuesta y/o solución de ellos a la brevedad posible o en su defecto gestionar su solución con la persona responsable del tema en cuestión.
 
                                            </p>
+                             
+			<center>
 
+			<div class="modal fade" id="miventana" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" data-hidden="true">
+								&times;
+							</button>
+							<h4>Aviso de privacidad integral</h4>
+						</div>
+						<div class="modal-body"  style='text-align:left align=center'  >
+						
+Universidad Tecnológica de Puebla, con domicilio en Antiguo Camino a La
+Resurrección 1002 - A, Zona Industrial, 72300 Puebla, Pue. y portal de internet
+http://evirtual.utpuebla.edu.mx/tickets/, es el responsable del uso y protección de sus
+datos personales, y al respecto le informamos lo siguiente:
+<h4>¿Para qué fines utilizaremos sus datos personales?</h4> 
+Los datos personales que recabamos de usted, los utilizaremos para las siguientes
+finalidades que son necesarias para el servicio que solicita:
+Respuesta a mensajes del formulario de contacto, Prestación de cualquier servicio
+solicitado
+Solicitar correo electrónico para la verificación de su ticket reportado., Medio de
+comunicación sobre situaciones relacionadas al sistema al usuario.
+<h4>¿Qué datos personales utilizaremos para estos fines?</h4>
+Para llevar a cabo las finalidades descritas en el presente aviso de privacidad,
+utilizaremos los siguientes datos personales:
+Datos de identificación y contacto
+
+<h4>¿Cómo puede acceder, rectificar o cancelar sus datos
+personales, u oponerse a su uso o ejercer la revocación de
+consentimiento?</h4>
+Usted tiene derecho a conocer qué datos personales tenemos de usted, para qué los
+utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su
+derecho solicitar la corrección de su información personal en caso de que esté
+desactualizada, sea inexacta o incompleta (Rectificación); que la eliminemos de
+nuestros registros o bases de datos cuando considere que la misma no está siendo
+utilizada adecuadamente (Cancelación); así como oponerse al uso de sus datos
+personales para fines específicos (Oposición). Estos derechos se conocen como
+derechos ARCO.
+Para el ejercicio de cualquiera de los derechos ARCO, debe enviar una petición vía
+correo electrónico a evirtual@utpuebla.edu.mx y deberá contener:
+<li>Nombre completo del titular.</li> 
+<li>Domicilio.</li> 
+<li>Teléfono.</li> 
+<li> Correo electrónico usado en este sitio web.</li>
+<li>Asunto «Derechos ARCO»</li>
+Descripción el objeto del escrito, los cuales pueden ser de manera enunciativa más
+no limitativa los siguientes: Revocación del consentimiento para tratar sus datos
+personales; y/o Notificación del uso indebido del tratamiento de sus datos personales;
+y/o Ejercitar sus Derechos ARCO, con una descripción clara y precisa de los datos a
+Acceder, Rectificar, Cancelar o bien, Oponerse. En caso de Rectificación de datos
+personales, deberá indicar la modificación exacta y anexar la documentación soporte;
+es importante en caso de revocación del consentimiento, que tenga en cuenta que no
+en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata,
+ya que es posible que por alguna obligación legal requiramos seguir tratando sus
+datos personales. Asimismo, usted deberá considerar que para ciertos fines, la
+revocación de su consentimiento implicará que no le podamos seguir prestando el
+servicio que nos solicitó, o la conclusión de su relación con nosotros.
+<h4>¿En cuántos días le daremos respuesta a su solicitud?</h4>
+5 días
+<h4>d) ¿Por qué medio le comunicaremos la respuesta a su solicitud?</h4>
+
+Al mismo correo electrónico que se proporciona a generar un reporte o si está dentro
+del sistema, mediante su correo electrónico de registro.
+El uso de tecnologías de rastreo en nuestro portal de
+internet
+Le informamos que en nuestra página de internet utilizamos cookies, web beacons u
+otras tecnologías, a través de las cuales es posible monitorear su comportamiento
+como usuario de internet, así como brindarle un mejor servicio y experiencia al
+navegar en nuestra página. Los datos personales que obtenemos de estas
+tecnologías de rastreo son los siguientes:
+Identificadores, nombre de usuario y contraseñas de sesión, Búsquedas realizadas
+por un usuario
+Estas cookies, web beacons y otras tecnologías pueden ser deshabilitadas. Para
+conocer cómo hacerlo, consulte el menú de ayuda de su navegador. Tenga en cuenta
+que, en caso de desactivar las cookies, es posible que no pueda acceder a ciertas
+funciones personalizadas en nuestro sitio web.
+<h4>¿Cómo puede conocer los cambios en este aviso de
+privacidad?</h4>
+
+El presente aviso de privacidad puede sufrir modificaciones, cambios o
+actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias
+necesidades por los productos o servicios que ofrecemos; de nuestras prácticas de
+privacidad; de cambios en nuestro modelo de negocio, o por otras causas. Nos
+comprometemos a mantener actualizado este aviso de privacidad sobre los cambios
+que pueda sufrir y siempre podrá consultar las actualizaciones que existan en el sitio
+web <a href="http://evirtual.utpuebla.edu.mx/tickets/"> http://evirtual.utpuebla.edu.mx/tickets/</a>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-primary" data-dismiss="modal">
+								cerrar
+							</button>
+						</div>				
+					</div>
+				</div>
+			</div>
                                 </article>
                              </section>
 
